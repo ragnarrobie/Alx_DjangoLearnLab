493c0dc5562662ee2b645971e59c0af97349d21d
Robson Taye <ragnarrobie@gmail.com>
2025-09-06 12:01:14 +0300

    5

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -17,16 +17,15 @@ except Author.DoesNotExist:
 
 print("\n" + "-"*40 + "\n")
 library_name = "Central Library"
-# Retrieve the librarian for a library
 try:
     library = Library.objects.get(name=library_name)
-    librarian = Librarian.objects.get(library=library)  # <- This line
-    print(f"Librarian of {library_name}: {librarian.name}")
+    librarian = Librarian.objects.get(library=library)
+    books_in_library = library.books.all()
+    print(f"Books in {library_name}:")
+    for book in books_in_library:
+        print(f"- {book.title} (by {book.author.name})")
 except Library.DoesNotExist:
     print(f"No library found with the name {library_name}")
-except Librarian.DoesNotExist:
-    print(f"No librarian assigned to {library_name}")
-
 
 print("\n" + "-"*40 + "\n")
 try:
