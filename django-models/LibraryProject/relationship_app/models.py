1525102f44caae2243a5094a051d0b8676016056
Robson Taye <ragnarrobie@gmail.com>
2025-09-13 17:12:38 +0300

    2

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,35 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-    def __str__(self):
-        return self.name
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey (Author, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.title
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
-    def __str__(self):
-        return self.name
-class Librarian(models.Model):
-    name= models.CharField(max_length=100)
-    library = models.OneToOneField(Library,on_delete=models.CASCADE)
-    def __str__(self):
-        return self.name
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)  # assuming author is a User
-    publication_year = models.IntegerField()
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return self.title
+        return f"{self.user.username} - {self.role}"
+
+
+# Automatically create or update UserProfile whenever a User is created
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
