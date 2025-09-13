1bd9b5865864efd09bb39774903f53465217afa5
Robson Taye <ragnarrobie@gmail.com>
2025-09-13 16:12:27 +0300

    1

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+# Get the custom user model
+User = get_user_model()
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    published_date = models.DateField(null=True, blank=True)
+    # Link book to a user (e.g., who added it)
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
-        return f"{self.title} by {self.author}({self.publication_year})"
+        return self.title
