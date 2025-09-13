2b147dbd7e420f46460d479450a14837d146bf8f
Robson Taye <ragnarrobie@gmail.com>
2025-09-13 16:43:01 +0300

    1

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm
+from .forms import BookForm,ExampleForm
 
 # List all books (requires can_view)
 @permission_required('bookshelf.can_view', raise_exception=True)
