91d63bf5b22d57478c5fb1de78ad127dd12d6f34
Robson Taye <ragnarrobie@gmail.com>
2025-10-05 18:40:05 +0300

    2

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,3 +10,19 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    # User authentication routes
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    
+    # User profile
+    path('profile/', views.profile, name='profile'),
+
+    # (Optional) homepage or blog index
+    # path('', views.home, name='home'),
+]
