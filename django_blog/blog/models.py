867ebc7ffb24a308ad09423b9414a1468937ba3e
Robson Taye <ragnarrobie@gmail.com>
2025-10-05 18:46:17 +0300

    2

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -32,22 +32,24 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         instance.profile.save()
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    # optional image for posts
-    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
-    date_posted = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-
-    class Meta:
-        ordering = ['-date_posted']
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)  # automatically set when created
+    updated_at = models.DateTimeField(auto_now=True)      # automatically update on save
 
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        # Used by CreateView/UpdateView for redirect after save
-        return reverse('post-detail', kwargs={'pk': self.pk})
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)  # timestamp when created
+    updated_at = models.DateTimeField(auto_now=True)      # timestamp when updated
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
