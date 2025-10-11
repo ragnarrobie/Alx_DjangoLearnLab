b56f8a5e7931c0fc8802569e52a7e0ad4a6a9f08
Robson Taye <ragnarrobie@gmail.com>
2025-10-11 17:30:16 +0300

    2

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -10,13 +10,15 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)  # <-- explicitly use CharField
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        # <-- use get_user_model().objects.create_user explicitly
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
